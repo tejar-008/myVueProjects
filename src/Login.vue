21c23e1003049a481838e2dce35fcee651acf6ba
tejar008 <60283073+tejar-008@users.noreply.github.com>
2021-08-16 08:59:43 +0530

    Add files via upload

---
+++ src/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <h1>Hello Guys</h1>
+    <h2>How are you all!!!</h2>
+    <h3>Please Login here!!</h3>
+    <form action="/" method="POST">
+        <label for="email"> Email</label><input name="email" class="email" type="email">
+        <br>
+        <br>
+        <label for="password"> Password</label><input name="password" class="password" type="password">
+        <br>
+        <br>
+        <button type="submit">Login</button>
+    </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"App1",
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+// import App from './App.vue'
+import App1 from './Login.vue'
+
+// createApp(App).mount('#app')
+createApp(App1).mount('#app')
